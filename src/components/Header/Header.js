c72ff2d36cc8e541dc2f1c97c458a70da788d3e0
BushraAzmatHussain <bushrahussain407@gmail.com>
2021-10-05 12:29:23 +0600

    final push

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-             <h1 className=" mt-4 p-4">WElcome to CSE course center</h1> 
+             <h1 className=" mt-4 p-4">CSE course center</h1> 
         </div>
     );
 };
